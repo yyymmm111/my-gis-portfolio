264a6f87b4d9b11860c1aec03f259e3037dde4d2
yyymmm111 <3361767663@qq.com>
2026-06-20 23:16:04 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   base: '/my-gis-portfolio/', // 必须和GitHub仓库名称完全一致
+  build: {
+    outDir: "docs" // 打包产物输出到docs文件夹
+  },
   plugins: [vue()],
   server: {
     port: 5178,
